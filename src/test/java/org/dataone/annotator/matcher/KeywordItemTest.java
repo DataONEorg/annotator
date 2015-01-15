e6363694af59fd37867046ae0fb16244c46d7924
iloveleon <xixiluo.china@gmail.com>
2015-01-15 16:17:06 -0500

    added two tests classes

---
+++ src/test/java/org/dataone/annotator/matcher/KeywordItemTest.java
@@ -0,0 +1,21 @@
+package org.dataone.annotator.matcher;
+
+import org.junit.Test;
+
+/**
+ * Created by xixiluo on 1/15/15.
+ */
+public class KeywordItemTest {
+
+    /**
+     * constructor for the test
+     */
+    public KeywordItemTest(){
+        
+    }
+
+    @Test
+    public void testToString(){
+
+    }
+}
